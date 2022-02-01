89dc6e60466240741d1166bc2773a8aa6e776952
d0nde1i0n <96944829+d0nde1i0n@users.noreply.github.com>
2022-02-01 18:54:14 +0900

    [End] DWC javascript practice

---
+++ DWC-javascript/app.js
@@ -0,0 +1,12 @@
+let num_1 = 6;
+let num_2 = 3
+
+sum = num_1 + num_2;
+diff = num_1 - num_2;
+prod = num_1 * num_2;
+quot = num_1 / num_2;
+alert(
+  'num_1 + num_2は'+sum+','
+  +'num_1 - num_2は'+diff+','
+  +'num_1 * num_2は'+prod+','
+  +'num_1 / num_2は'+quot+'です。');
